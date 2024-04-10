a7040c391d51cf694de59098af779ab036a15e78
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-04-10 00:18:19 -0300

    Module 02-Aula 09

--- src/componentes/BarraNavegacao/index.tsx
+++ src/componentes/BarraNavegacao/index.tsx
@@ -1,14 +1,56 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import BotaoNavegacao from "../BotaoNavegacao"
 import ModalCadastroUsuario from "../ModalCadastroUsuario"
+import ModalLoginUsuario from "../ModalLoginUsuario"
 import logo from './assets/logo.png'
 import usuario from './assets/usuario.svg'
 import './BarraNavegacao.css'
-import { useState } from "react"
 
 const BarraNavegacao = () => {
 
     const [modalCadastroAberta, setModalCadastroAberta] = useState(false)
+    const [modalLoginAberta, setModalLoginAberta] = useState(false)
+    const [usuarioEstaLogado, setUsuarioEstaLogado] = useState(false)
+
+    const aoEfetuarLogin = () => {
+        setUsuarioEstaLogado(true)
+        setModalLoginAberta(false)
+    }
+
+    const acoesQuandoDeslogado = (<>
+        <li>
+            <BotaoNavegacao
+                texto="Login"
+                textoAltSrc="Icone representando um usuário"
+                imagemSrc={usuario}
+                onClick={() => setModalLoginAberta(true)}
+            />
+            <ModalLoginUsuario
+                aberta={modalLoginAberta}
+                aoFechar={() => setModalLoginAberta(false)}
+                aoEfetuarLogin={aoEfetuarLogin}
+            />
+        </li>
+        <li>
+            <BotaoNavegacao
+                texto="Cadastrar-se"
+                textoAltSrc="Icone representando um usuário"
+                imagemSrc={usuario}
+                onClick={() => setModalCadastroAberta(true)}
+            />
+            <ModalCadastroUsuario
+                aberta={modalCadastroAberta}
+                aoFechar={() => setModalCadastroAberta(false)}
+            />
+        </li>
+    </>)
+
+    const acoesQuandoLogado = (<>
+        <li>
+            <Link to="/minha-conta/pedidos">Minha Conta</Link> 
+        </li>
+    </>)
 
     return (<nav className="ab-navbar">
         <h1 className="logo">
@@ -49,22 +91,8 @@ const BarraNavegacao = () => {
             </li>
         </ul>
         <ul className="acoes">
-            <li>
-                <BotaoNavegacao texto="Login" textoAltSrc="Icone representando um usuário" imagemSrc={usuario} />
-            </li>
-            <li>
-                <BotaoNavegacao
-                    texto="Cadastrar-se"
-                    textoAltSrc="Icone representando um usuário"
-                    imagemSrc={usuario}
-                    onClick={() => setModalCadastroAberta(true)}
-                />
-              <ModalCadastroUsuario
-                aberta={modalCadastroAberta}
-                aoFechar={() => setModalCadastroAberta(false)}
-            />
-            </li>
-        </ul>
+            {usuarioEstaLogado ? acoesQuandoLogado : acoesQuandoDeslogado}
+        </ul>   
     </nav>)
 }
 
